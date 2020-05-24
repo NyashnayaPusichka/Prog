224f483a726cae336ca5c6440db7dbb62ebde045
mxcv <64147945+mxcv@users.noreply.github.com>
2020-05-25 01:12:43 +0300

    Update lr13_v15.c

--- lr13_v15.c
+++ lr13_v15.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 #include <conio.h>
+#include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
 
 int main()
 {
-	char str[128];
+	char str[128], num[sizeof(str) * 2];
+	*num = '\0';
 	printf("String: ");
 	gets_s(str, sizeof(str));
-	for( int i = 0; i < '\0'; i++)
 	
+	for(int i = 0, i2 = 0; str[i] != '\0'; ++i)
+		if (isdigit(str[i]))
+		{
+			// TODO
+		}
+	
+	printf("Result: %s", num)
 	_getch();
 	return 0;
 }
