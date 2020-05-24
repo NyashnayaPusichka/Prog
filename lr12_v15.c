f90f189b950c495d5222fe77fa3bf4f2ad072d9d
RobbieRotten <61269499+NyashnayaPusichka@users.noreply.github.com>
2020-05-24 21:29:20 +0300

    Update lr12_v15.c

--- lr12_v15.c
+++ lr12_v15.c
@@ -1,10 +1,36 @@
 #include <stdio.h>
 #include <conio.h>
-#define MAX = 128
+#define MAX 128
+
+int sort_dec(float* arr, int n);
 
 main()
 {
-	float m[128];
+	float m[MAX];
+	unsigned int i, n;
+	printf("n = "); scanf_s("%i", &n);
+	if (n > 0 && n < MAX)
+	{
+		for (i = 0; i < n; i++)
+		{
+			printf("m1[%i] = ", i);
+			scanf_s("%i", &m[i]);
+		}
+	}
+	else printf("Wrong number");
 	_getch();
 	return 0;
+}
+
+int sort_dec(float* arr, int n)
+{
+	float t;
+	for (int i = 0; i < n - 1; i++)
+		for (int j = 0; j < n - i - 1; j++)
+			if (arr[j] < arr[j + 1])
+			{
+				t = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = t;
+			}
 }
