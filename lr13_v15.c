639355d8b9724223b373406cbfb85648478c9fb3
RobbieRotten <61269499+NyashnayaPusichka@users.noreply.github.com>
2020-05-25 00:38:46 +0300

    Update lr13_v15.c

--- lr13_v15.c
+++ lr13_v15.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX 128
 
+main()
+{
+	const char enter = '\r';
+	char s[MAX];
+	printf("s=");
+	gets_s(s, MAX);
+	_getch();
+	return 0;
+}
